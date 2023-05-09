755dc858d92f7dfeaa03b2e0f2ccaf0d9ee18cc0
Andy Cavedal <hcavedal@gmail.com>
2023-05-09 15:48:58 -0300

    Home brief introduction added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import '@styles/Home.scss';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Hello world!</h1>
+        <div class="title-container">
+            <h1 id='title-main'>Catan map generator and point tracker</h1>
+            <h2 id='title-secondary'>This is a proyect for fun, made with React18. If you want to collabore with me for a proyect check the Contact section in the right-top of the header.</h2>
         </div>
     );
 }
